52244017cc37efd8e4acbd6218f63640d78d10a9
Kapilm2599 <mahajankapil575@gmail.com>
2023-04-19 13:09:14 +0530

    Added: Create contact in addressbook

---
+++ src/com/blz/addressBook/AddressBookMain.java
@@ -0,0 +1,12 @@
+package com.blz.addressBook;
+
+public class AddressBookMain {
+	
+	public static void main(String[] args) {
+
+		System.out.println("Welcome To Address Book system Program");
+		ContactPerson contactperson = new ContactPerson(); 
+		System.out.println(contactperson.toString());
+	}
+
+}
---
+++ src/com/blz/addressBook/ContactPerson.java
@@ -0,0 +1,32 @@
+package com.blz.addressBook;
+
+public class ContactPerson {
+	
+	private String firstName, lastName, email, address, city, state;
+	private int zip;
+	private long mobile;
+
+	ContactPerson() {
+	}
+
+	public ContactPerson(String firstName, String lastName, String email, String address, String city, String state,
+			int zip, long mobile) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.mobile = mobile;
+	}
+
+	public String toString() {
+		return "ContactPerson [firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + ", address="
+				+ address + ", city=" + city + ", state=" + state + ", zip=" + zip + ", mobile=" + mobile
+				+ ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString()
+				+ "]";
+	}
+
+}

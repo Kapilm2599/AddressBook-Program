025f9de6ab4331dd253cac84305acc1e55773d26
Kapilm2599 <mahajankapil575@gmail.com>
2023-04-19 13:32:15 +0530

    Added: Ability to add new contact to addressbook

--- src/com/blz/addressBook/ContactPerson.java
+++ src/com/blz/addressBook/ContactPerson.java
@@ -5,28 +5,60 @@ public class ContactPerson {
 	private String firstName, lastName, email, address, city, state;
 	private int zip;
 	private long mobile;
-
-	ContactPerson() {
+	
+	public String getFirstName() {
+		return firstName;
 	}
-
-	public ContactPerson(String firstName, String lastName, String email, String address, String city, String state,
-			int zip, long mobile) {
-		super();
+	public void setFirstName(String firstName) {
 		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
 		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
 		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
 		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
 		this.state = state;
+	}
+	public int getZip() {
+		return zip;
+	}
+	public void setZip(int zip) {
 		this.zip = zip;
+	}
+	public long getMobile() {
+		return mobile;
+	}
+	public void setMobile(long mobile) {
 		this.mobile = mobile;
 	}
-
+	
+	@Override
 	public String toString() {
 		return "ContactPerson [firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + ", address="
-				+ address + ", city=" + city + ", state=" + state + ", zip=" + zip + ", mobile=" + mobile
-				+ ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString()
-				+ "]";
+				+ address + ", city=" + city + ", state=" + state + ", zip=" + zip + ", mobile=" + mobile + "]";
 	}
-
+	
 }
